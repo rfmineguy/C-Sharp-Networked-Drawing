cd0c503c06d2817c5e19bf4301f0c6e263ead0b5
rileyfischer <rfmineguy@gmail.com>
2026-01-29 22:22:02 -0800

    Implement base for Client->Server message handler, and base for Server->Client message handler.

---
+++ Client/MessageHandler.cs
@@ -0,0 +1,84 @@
+﻿using MessagePack;
+using System.Net.Sockets;
+using Common;
+
+namespace Client
+{
+    internal class MessageHandler
+    {
+        private ClientState _clientState;
+        private MessagePackStreamReader _reader;
+        private NetworkStream _stream;
+
+        public MessageHandler(ref ClientState clientState)
+        {
+            ArgumentNullException.ThrowIfNull(clientState);
+            ArgumentNullException.ThrowIfNull(clientState.client);
+            _clientState = clientState;
+            _stream = clientState.client.GetStream();
+            _reader = new MessagePackStreamReader(_stream);
+        }
+
+        public async Task Run (CancellationToken token)
+        {
+            Console.WriteLine("Client message handler started...");
+            bool running = true;
+            try
+            {
+                while (running)
+                {
+                    // if (token.IsCancellationRequested) break;
+                    Console.WriteLine("Message Handler running..");
+
+                    var readResult = await _reader.ReadAsync(token);
+                    if (readResult == null) break;
+
+                    Envelope e = MessagePackSerializer.Deserialize<Envelope>(readResult.Value);
+                    Message m = e.message;
+                    switch (m)
+                    {
+                        case SetGuid sg:
+                            if (_clientState.guid != null)
+                            {
+                                Console.WriteLine("Failed to set guid, already set");
+                                break;
+                            }
+                            _clientState.guid = sg.guid;
+                            Console.WriteLine($"Set client guid {_clientState.guid}");
+                            break;
+                        case Test t:
+                            break;
+                        case MouseMove mouse:
+                            Console.WriteLine($"{e.guid} moved to {mouse.x}, {mouse.y}");
+                            break;
+                        case UserConnection userConnection:
+                            switch (userConnection.type)
+                            {
+                                case UserConnection.ConnectionType.CONNECT:
+                                    _clientState.clients.Add(e.guid, new ClientData());
+                                    break;
+                                case UserConnection.ConnectionType.DISCONNECT:
+                                    Console.WriteLine("Disconnect response");
+                                    _clientState.clients.Remove(e.guid);
+                                    running = false;
+                                    break;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine($"Not a derived message class: \"{m.ToString()}\"");
+                            Console.WriteLine(e);
+                            break;
+                    }
+                }
+            } catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            _reader.Dispose();
+            _stream.Dispose();
+            _clientState.client.Close();
+
+            Console.WriteLine("Client message handler ended...");
+        }
+    }
+}
--- Server/ClientHandler.cs
+++ Server/ClientHandler.cs
@@ -1,27 +1,58 @@
 ﻿using System.Net.Sockets;
+using Common;
+using MessagePack;
 
 namespace Networked_Drawing
 {
     internal class ClientHandler
     {
         private TcpClient _client;
+        private ServerState _serverState;
+        private NetworkStream _stream;
+        private MessagePackStreamReader _reader;
+        private CancellationTokenSource _cancelTokenSource;
+        public Guid _uuid;
         
-        public ClientHandler(TcpClient client) {
+        public ClientHandler(TcpClient client, ServerState serverState) {
             _client = client;
-            Console.WriteLine("Client connected");
+            _serverState = serverState;
+            _stream = _client.GetStream();
+            _reader = new MessagePackStreamReader(_stream);
+            _cancelTokenSource = new CancellationTokenSource();
+            _uuid = Guid.NewGuid();
         }
 
-        public void Run()
+        public void SendMessage(Message message)
         {
-            while (true)
+            Envelope env = new Envelope(_uuid, message);
+            MessagePack.MessagePackSerializer.Serialize<Envelope>(this._stream, env);
+            this._stream.Flush();
+        }
+
+        public async Task Run()
+        {
+            Console.WriteLine("Client listener started...");
+            SendMessage(new SetGuid(_uuid));
+            Console.WriteLine($"Sending guid: {_uuid}");
+            bool running = true;
+            while (running)
             {
-                if (_client.Client.Poll(0, SelectMode.SelectRead) && _client.Client.Available == 0) break;
-                
-                Console.WriteLine("Looping");
+                if (_reader == null) continue;
+                if (_cancelTokenSource.IsCancellationRequested) break;
+
+                var readResult = await _reader.ReadAsync(_cancelTokenSource.Token);
+                if (readResult == null) break;
+
+                // read messages
+                Envelope e = MessagePackSerializer.Deserialize<Envelope>(readResult.Value);
+                Message m = e.message;
+                Console.WriteLine($"Received: {e}");
             }
 
-            Console.WriteLine("Client disconnected");
+            // _serverState.Broadcast(this, new UserConnection(UserConnection.ConnectionType.DISCONNECT));
+            Console.WriteLine($"Client listener ended... {this._uuid}");
             _client.Close();
+            _serverState.clients.Remove(this);
         }
     }
 }
